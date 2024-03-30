a8a88e5604e08289a9b9a1f67114421d8c9a0a23
ShaikAshrafAli27 <ashrafshaik617@gmail.com>
2024-03-31 02:19:36 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,9 +23,11 @@ function Hero() {
               variants={container(0)}
               initial="hidden"
               animate="visible"
-              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+              className="pb-16 text-6xl font-thin  tracking-tight lg:mt-16 lg:text-8xl"
             >
-              Asharf Ali
+              <span className="font-['Caveat']">
+                Shaik <br /> Ashraf Ali
+              </span>
             </motion.h1>
             <motion.span
               variants={container(0.5)}
